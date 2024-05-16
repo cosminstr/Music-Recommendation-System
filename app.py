552decd40e7e040715301437e67610a5fdd71fe4
cosminstercu <cosminstercu@gmail.com>
2024-05-16 22:41:28 +0300

    scaled down the values before applying Kmeans

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import spotipy
 import pandas as pd
+import numpy as np
 import json
 from credentials import *
 from spotipy.oauth2 import SpotifyOAuth
+from sklearn.preprocessing import StandardScaler
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
@@ -10,9 +12,17 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                scope=MY_SCOPES))
 
 # DATA PREPROCESSING
-music_data = pd.read_csv('D:\Projects\Music Recommendation System\dataset.csv')
-music_data["explicit"] = music_data["explicit"].astype(int)
+music_data: pd.DataFrame = pd.read_csv('D:\Projects\Music Recommendation System\spotify_dataset.csv')
 music_data = music_data.drop_duplicates()
 music_data = music_data.dropna(axis=0)
 
-print(music_data)
+scaler = StandardScaler()
+music_data = music_data.select_dtypes(np.number)
+
+
+scaled_X = scaler.fit_transform(music_data.values)
+
+scaled_music_data = pd.DataFrame(scaled_X,
+                        columns=music_data.columns)
+
+
