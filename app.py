0c48b24c98254bc44bd2f20252867081f24df9cf
cosminstercu <cosminstercu@gmail.com>
2024-05-16 20:41:46 +0300

    hid spotipy credentials

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import spotipy
 import pandas as pd
 import json
+from credentials import *
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='5b842c9738454b9a8ed6da1b3daffa31',
-                                               client_secret='64222ed18a944e0daeca1e6124819e7a',
-                                               redirect_uri='http://localhost:8889/callback',
-                                               scope='playlist-read-private'))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope=MY_SCOPES))
 
 # DATA PREPROCESSING
 music_data = pd.read_csv('D:\Projects\Music Recommendation System\dataset.csv')
