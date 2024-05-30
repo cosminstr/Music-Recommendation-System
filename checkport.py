c5aa69cd4fb85a4dff6e6681c6e1400c73151a53
cosmin <150474445+cosminstr@users.noreply.github.com>
2024-05-30 20:43:03 +0300

    added comment to checkport.py

--- checkport.py
+++ checkport.py
@@ -1,6 +1,6 @@
 # this file was used in the early stages when choosing the port for 
 # the REDIRECT_URI from the Spotify API. Port 3000 is not working on my machine 
-# as i already have another backgorund process at this port, therefore i chose
+# as i already have another backgorund process at this port, as such i chose
 # a different one.
 
 import psutil
